6156e33a945045c895ccbc331578c02a09513fd9
abhishekjadhav3470 <abhishekjadhav3470@gmail.com>
2023-11-26 19:09:58 +0530

    stage_01_load_and_save_completed

--- src/stage_01_load_and_save.py
+++ src/stage_01_load_and_save.py
@@ -1,18 +1,27 @@
 import pandas as pd
 import argparse
-from src.utils.common_utils import read_params
+from src.utils.common_utils import read_params, create_dir,save_local_df, clean_prev_dirs_if_exists
 import logging
 
-logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
-logging.basicConfig(level=logging.DEBUG, format=logging_str)
+# logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+# logging.basicConfig(level=logging.DEBUG, format=logging_str)
 
 
 def get_data(config_path):
     config = read_params(config_path)
 
-    print(config)
+    data_path = config["data_source"]["uci_source"]
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    raw_local_data_dir = config["artifacts"]["raw_local_data_dir"]
+    raw_local_data = config["artifacts"]["raw_local_data"]
 
-    return None
+    clean_prev_dirs_if_exists(artifacts_dir)
+
+    create_dir(dirs=[artifacts_dir, raw_local_data_dir])
+
+    df = pd.read_csv(data_path, sep=";")
+
+    save_local_df(df, raw_local_data, header=True)
 
 
 if __name__ == "__main__":
@@ -22,7 +31,7 @@ if __name__ == "__main__":
 
     try:
         data = get_data(config_path=parsed_args.config)
-        #ogging.info(f"reading and writing raw data stage completed")
+        # logging.info(f"reading and writing raw data stage completed")
     except Exception as e:
-        # logging.error(e)
+        #logging.error(e)
          raise e
